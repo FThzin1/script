a4fecd9168cdcb17b2ebd513a5a0a53b324323b0
Thzin <108431267+FThzin1@users.noreply.github.com>
2022-12-14 00:09:12 -0300

    Update bungas.lua

--- bungas.lua
+++ bungas.lua
@@ -1,62 +1,46 @@
-
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-    local Player = game.Players.LocalPlayer
+-- It detect a SPAWNED fruit only if u are on the same island with that fruit. Also u must be atleast 300 meters away
 
 
-    local Window = OrionLib:MakeWindow({Name = "Bungas", HidePremium = false, SaveConfig = true, ConfigFolder = "Bungas"})
-    -- VALOR
-    _G.Velocidade = 30
-    _G.Velocidadeat = true
+-- Made by MrKix#1233
+-- Instances:
+local BullShit = Instance.new("ScreenGui")
+local MyFrame = Instance.new("Frame")
+local ESP = Instance.new("TextLabel")
+BullShit.Name = "BullShit"
+BullShit.Parent = game.CoreGui
+BullShit.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+MyFrame.Name = "MyFrame"
+MyFrame.Parent = BullShit
+MyFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+MyFrame.BackgroundTransparency = 1.000
+MyFrame.Position = UDim2.new(0.683068037, 0, 0.885974944, 0)
+MyFrame.Size = UDim2.new(0, 438, 0, 100)
+ESP.Name = "ESP"
+ESP.Parent = MyFrame
+ESP.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ESP.BackgroundTransparency = 0.400
+ESP.Position = UDim2.new(0.119, 0, 0.37, 0)
+ESP.Size = UDim2.new(0, 350, 0, 50)
+ESP.Font = Enum.Font.SourceSans
+ESP.Text = "Waiting for a fruit to detect..."
+ESP.TextColor3 = Color3.fromRGB(0, 0, 0)
+ESP.TextSize = 16.000
+script.Parent = ESP
+print('Made by MrKix#1233')
+local guranteedsuke = game.Workspace.Env.Settings:FindFirstChild('Folder')
+if guranteedsuke then 
+    for i, v in pairs(guranteedsuke:GetDescendants()) do 
+        if v:IsA("Model") then 
+            spawn(function()
+                while wait() and guranteedsuke.Parent ~= nil do
+                    local plr = game.Players.LocalPlayer
+                    local chara = plr.Character
+                    local fruitpos = v.PrimaryPart.Position
+                    local mypos = chara.HumanoidRootPart.Position
+                    ESP.Text = 'Distance: '..tostring(((fruitpos-mypos).Magnitude))
+                end
+            end)
+        end 
+    end 
+end
 
-
-    function Velocidade()
-        local Player = game:service'Players'.LocalPlayer;
-        Player.Character.Humanoid:GetPropertyChangedSignal'WalkSpeed':Connect(function()
-            Player.Character.Humanoid.WalkSpeed = _G.Velocidade;
-        end)
-        Player.Character.Humanoid.WalkSpeed = _G.Velocidade;
-    end
-
-    local JogadorTab = Window:MakeTab({
-    	Name = "Jogador",
-    	Icon = "rbxassetid://4483345998",
-    	PremiumOnly = false
-    })
-
-    local Section = JogadorTab:AddSection({
-        Name = "CHEAT FEITO POR THZIN"
-    })
-
-
-    
-    JogadorTab:AddButton({
-        Name = "Ativar Speed",
-        Default = false,
-        Callback = function(Value)
-            _G.Velocidadeat = Value
-            Velocidade()
-
-            
-            Player.RespawnTime = 1.0
-        end    
-    })  
-
-    JogadorTab:AddTextbox({
-        Name = "Controle da velocidade",
-        Default = 100,
-        TextDisappear = true,
-        Callback = function(Value)
-            _G.Velocidade = Value
-            Velocidade()
-
-        end	  
-    })
-
-    JogadorTab:AddToggle({
-        Name = "This is a toggle!",
-        Default = false,
-        Callback = function(Value)
-            print(Value)
-
-        end    
-    })
